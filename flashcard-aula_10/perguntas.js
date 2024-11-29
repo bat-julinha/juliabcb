c435ad515c840b74273013c2557f58d1ba82de36
bat-julinha <00001104450628sp@al.educacao.sp.gov.br>
2024-11-29 09:12:00 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual é a unidade básica da vida?',
+    'A célula'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'O oceano pacífico '
+)
+
+criaCartao(
+    'História',
+    'Em que ano foi proclamada a independência do Brasil?',
+    'Em 1822'
+)
+
+criaCartao(
+    'Física',
+    'O que é a lei da conservação da energia?',
+    'A energia não é criada nem destruída, apenas transformada'
+)
